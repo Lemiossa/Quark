20babdd210a5517919e43e122b6a6c6db00232bd
Matheus Leme Da Silva <matheuslemedasilva000@gmail.com>
2026-01-25 16:29:05 -0300

    Fix remap PIC2 offset

--- Source/Kernel/Main.c
+++ Source/Kernel/Main.c
@@ -3,11 +3,13 @@
  * Created by Matheus Leme Da Silva
  */
 #include "Gdt.h"
+#include "Pic.h"
 #include "Vga.h"
 
 // The Main function don't return
 void Main(void) {
   GdtInit();
+  PicRemap(0x20, 0x28);
   Puts("Hello Kernel!\r\n");
 
   for (;;)
