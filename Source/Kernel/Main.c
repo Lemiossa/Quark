db958f3675708f952510f1a85c5f6d79bd2c6ef8
Matheus Leme Da Silva <matheuslemedasilva000@gmail.com>
2026-01-28 09:46:11 -0300

    Kernel: Update on Main.c

--- Source/Kernel/Main.c
+++ Source/Kernel/Main.c
@@ -2,6 +2,7 @@
  * Main.c
  * Created by Matheus Leme Da Silva
  */
+#include "Types.h"
 #include "Vga.h"
 #include "Gdt.h"
 #include "Idt.h"
@@ -9,6 +10,7 @@
 #include "E820.h"
 #include "Pmm.h"
 #include "Serial.h"
+#include "Util.h"
 
 struct QuarkBootInfo {
 	struct E820Entry *E820Table;
@@ -19,16 +21,29 @@ struct QuarkBootInfo {
 void Main(struct QuarkBootInfo *bootInfo) {
 	SetCurrentAttr(MAKE_VGA_COLOR(WHITE, BLUE));
 	Clear();
-	GdtInit();
-	IdtInit();
-	SerialInit(COM1, 3);
-	PicRemap(0x20, 0x28);
-
 	E820Print(bootInfo->E820Table, bootInfo->E820EntryCnt);
 	PmmInit(bootInfo->E820Table, bootInfo->E820EntryCnt);
 
-	__asm__ volatile("sti");
+	GdtInit();
+	IdtInit();
+	if (SerialInit(COM1, 3) == 0) {
+		Puts("Initialized Serial port 0x");
+		PutHexU16(COM1);
+		Puts("\r\n");
+	}
+	PicRemap(0x20, 0x28);
+
+
 	Puts("Hello Kernel!\r\n");
+
+	Puts("Kernel size: 0x");
+	PutHexU32((U32)__END_ADDR - (U32)__START_ADDR);
+	Puts("\r\n");
+
+	void *ptr = (void *)PmmAllocPage();
+	Puts ("Allocated page: 0x");
+	PutHexU32((U32)ptr);
+	Puts("\r\n");
 
 	for (;;);
 }
